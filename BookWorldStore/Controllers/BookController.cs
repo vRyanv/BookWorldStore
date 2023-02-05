a370d4d22e5ae791c2ffb64590a72b26b2aa9b10
vv <89388361+vRyanv@users.noreply.github.com>
2023-02-05 23:51:07 +0700

    Admin book view and client index view

---
+++ BookWorldStore/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using BookWorldStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWorldStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly AppDBContext dbContext;
+        public BookController(AppDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/Book/Index.cshtml");
+        }
+
+        [HttpGet]
+        async public Task<IActionResult> Create()
+        {
+            return View("~/Views/Admin/Book/Create.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        async public Task<IActionResult> Edit()
+        {
+            return View("~/Views/Admin/Book/Edit.cshtml");
+        }
+    }
+}
