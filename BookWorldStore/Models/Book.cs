db91189762891a5e3288bdaa817f79f0ce6faf62
nhanhtgcc200004 <nhanhtgcc200004@fpt.edu.vn>
2023-02-06 12:31:52 +0700

    Update Book.cs
    tesst 00

--- BookWorldStore/Models/Book.cs
+++ BookWorldStore/Models/Book.cs
@@ -17,6 +17,8 @@ namespace BookWorldStore.Models
         public string title { get; set; }
         public string des { get; set; }
         public string author { get; set; }
+        public int inventory_num { get; set; }
+        public string image { get; set; }
         public DateTime publishing_year { get; set; }
         public float price { get; set; }
 
