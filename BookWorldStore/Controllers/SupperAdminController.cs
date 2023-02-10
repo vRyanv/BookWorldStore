408eaff8424b8d07451618ff573d5044f5b62747
vv <89388361+vRyanv@users.noreply.github.com>
2023-02-10 21:38:33 +0700

    Create Supplier admin view

---
+++ BookWorldStore/Controllers/SupperAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookWorldStore.Controllers
+{
+    public class SupperAdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Admin/SupperAdmin/Index.cshtml");
+        }
+    }
+}
