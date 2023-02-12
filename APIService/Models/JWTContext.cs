7891446a77f4771d4fcf456d6330a7690d637702
vv <89388361+vRyanv@users.noreply.github.com>
2023-02-12 09:01:43 +0700

    create API service

---
+++ APIService/Models/JWTContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace APIService.Models
+{
+    public class JWTContext : DbContext
+    {
+        public JWTContext(DbContextOptions<JWTContext> options) : base(options) { }
+        public DbSet<User> users { get; set; }
+    }
+}
---
+++ APIService/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace APIService.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
