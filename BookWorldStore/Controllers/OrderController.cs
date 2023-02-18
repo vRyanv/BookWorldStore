35530e67283159daa34e77f4f95cca65fd656f2d
nhanhtgcc200004 <nhanhtgcc200004@fpt.edu.vn>
2023-02-18 17:38:03 +0700

    view book and view order admin

--- BookWorldStore/Controllers/OrderController.cs
+++ BookWorldStore/Controllers/OrderController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookWorldStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookWorldStore.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly AppDBContext dbContext;
+        public OrderController(AppDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
         public IActionResult Index()
         {
-            return View("~/Views/Admin/Order/Index.cshtml");
+            var result=dbContext.orders.Include("user").ToList();
+            return View("~/Views/Admin/Order/Index.cshtml",result);
         }
     }
 }
