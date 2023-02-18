9c2c74e4e365cd9eed646ccaa73fce307c840a09
nhanhtgcc200004 <nhanhtgcc200004@fpt.edu.vn>
2023-02-18 18:47:06 +0700

    order detail

--- BookWorldStore/Controllers/OrderController.cs
+++ BookWorldStore/Controllers/OrderController.cs
@@ -16,5 +16,11 @@ namespace BookWorldStore.Controllers
             var result=dbContext.orders.Include("user").ToList();
             return View("~/Views/Admin/Order/Index.cshtml",result);
         }
+        
+        public async Task<IActionResult> OrderDetail(int id)
+        {
+            var result= await dbContext.orderDetails.Include("order").Include("book").Where(od=>od.order.order_id==id).ToListAsync();
+            return View("~/Views/Admin/Order/OrderDetail.cshtml", result);
+        }
     }
 }
