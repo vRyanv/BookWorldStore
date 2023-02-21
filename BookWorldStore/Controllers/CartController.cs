c50c07a09617787a8834fe3463f2c0fa98dacb49
vv <89388361+vRyanv@users.noreply.github.com>
2023-02-21 11:55:26 +0700

    create index cart

--- BookWorldStore/Controllers/CartController.cs
+++ BookWorldStore/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using BookWorldStore.Utils;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace BookWorldStore.Controllers
@@ -16,21 +17,70 @@ namespace BookWorldStore.Controllers
         }
 
         [Authorize(Roles = "client, owner, admin")]
-        public IActionResult Index()
-        {
-            User user = UserUtils.Instance.GetUser(HttpContext);
-            
-            return View();
-        }
-
-        [Authorize(Roles = "client, owner, admin")]
-        public IActionResult AddToCart(int id)
+        public async Task<IActionResult> Index()
         {
             int userId = UserUtils.Instance.GetUser(HttpContext).user_id;
-            var result = dbContext.orders.Where(o => o.user_id == userId && o.status == 0);
-            Order order = new Order();
-            
-            order.user_id = id;
+            Order order = await dbContext.orders.Where(o => o.user_id == userId && o.status == 0).FirstOrDefaultAsync();
+            List<OrderDetail> orderList = null;
+            if (order == null)
+            {
+                Order newOrder = new Order();
+                newOrder.user_id = userId;
+                newOrder.status = 0;
+                dbContext.Add(newOrder);
+                await dbContext.SaveChangesAsync();
+                orderList = await dbContext.orderDetails.Where(o => o.order_id == newOrder.order_id).ToListAsync();
+            }
+            else
+            {
+                orderList = await dbContext.orderDetails.Where(o => o.order_id == order.order_id).ToListAsync();
+            }
+            return View(orderList);
+        }
+        
+        [HttpGet]
+        [Authorize(Roles = "client, owner, admin")]
+        public async Task<IActionResult> AddToCart(int id)
+        {
+            int userId = UserUtils.Instance.GetUser(HttpContext).user_id;
+            Order order = await dbContext.orders.Where(o => o.user_id == userId && o.status == 0).FirstOrDefaultAsync();
+           
+            if (order != null)
+            {
+                OrderDetail orderDetail = await dbContext.orderDetails
+                                .Where(od => od.order_id == order.order_id && od.book_id == id)
+                                .FirstOrDefaultAsync();
+                if(orderDetail != null)
+                {
+                    orderDetail.quantity += 1;
+                }
+                else
+                {
+                    OrderDetail newOrderDetail = new OrderDetail();
+                    newOrderDetail.order_id = order.order_id;
+                    newOrderDetail.book_id = id;
+                    newOrderDetail.quantity = 1;
+                    dbContext.Add(newOrderDetail);
+                }
+                dbContext.SaveChangesAsync();
+
+            }
+            else
+            {
+                order = new Order();
+                order.user_id = userId;
+                order.status = 0;
+                dbContext.Add(order);
+                await dbContext.SaveChangesAsync();
+
+                OrderDetail newOrderDetail = new OrderDetail();
+                newOrderDetail.order_id = order.order_id;
+                newOrderDetail.book_id = id;
+                newOrderDetail.quantity = 1;
+                dbContext.Add(newOrderDetail);
+                await dbContext.SaveChangesAsync();
+
+            }
             return RedirectToAction("Index");
         }
 
