c6fee60a0706b828e2d945e1d9019fbbadff07c0
thaihpgcc200197 <thaihpgcc200197@fpt.edu.vn>
2023-02-26 21:01:31 +0700

    Add controller

---
+++ BookWorldStore/Controllers/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookWorldStore.Controllers
+{
+    public class User : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
