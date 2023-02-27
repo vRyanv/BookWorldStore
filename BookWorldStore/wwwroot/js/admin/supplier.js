9a74451f41adbed911d06903bfa21c71dcc93112
thaihpgcc200197 <thaihpgcc200197@fpt.edu.vn>
2023-02-27 17:42:59 +0700

    update supplier.js

--- BookWorldStore/wwwroot/js/admin/supplier.js
+++ BookWorldStore/wwwroot/js/admin/supplier.js
@@ -4,11 +4,19 @@
             e.preventDefault()
             $("#empty_name").show()
         }
+        if ($("#txt_sup_address").val().trim() === "") {
+            e.preventDefault()
+            $("#empty_address").show()
+        }
     })
-    $("#Edit_category_form").submit(function (e) {
+    $("#Edit_supplier_form").submit(function (e) {
         if ($("#txt_sup_name_edit").val().trim() === "") {
             e.preventDefault()
             $("#empty_name_edit").show()
+        }
+        if ($("#txt_sup_address_edit").val().trim() === "") {
+            e.preventDefault()
+            $("#empty_address_edit").show()
         }
     })
 })
